9256ebf0103c1610f319c4a74d401b53f512cac9
mulutse2022 <mulutsepay@gmail.com>
2022-12-10 11:11:46 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *vReturn: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+int num;
+for(num = '0'; num <= '9'; num++)
+putchar(num);
+putchar('\n');
+return (0);
+}
+
